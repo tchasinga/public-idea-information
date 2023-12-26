714a354a8fadba9f540589ebbe3e5fc95af5f28b
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-26 18:25:28 +0200

    code is added

--- frontend/app/tickets/[id]/page.jsx
+++ frontend/app/tickets/[id]/page.jsx
@@ -1,39 +1,40 @@
-import { notFound } from "next/navigation"
-
-
-
-async function getTicket(id) {
-  const res = await fetch(`http://localhost:5000/api/getId/${id}`, {
-    next: {
-      revalidate: 0 // use 0 to opt out of using cache
-    }
-  })
-
-  if (!res.ok) {
-    notFound()
+async function getTicket(_id) {
+  try {
+      await new Promise(resolve => setTimeout(resolve, 5000))
+      const res = await fetch(`http://localhost:5000/api/getId/${_id}`, {
+          next: {
+              revalidate: 0 // use 0 to opt out of using cache which means that refetch data 
+            }
+      });
+    return res.json();
+  } catch (error) {
+    console.error('Error fetching ticket:', error);
+    throw error; // Re-throw the error to propagate it to the caller
   }
-
-  return res.json()
 }
 
 
 export default async function TicketDetails({ params }) {
-  // const id = params.id
-  const ticket = await getTicket(params.id)
+  const ticket = await getTicket(params.id);
 
   return (
-    <main>
-      <nav>
-        <h2>Ticket Details</h2>
-      </nav>
-      <div className="card">
-        <h3>{ticket.title}</h3>
-        <small>Created by {ticket.user_email}</small>
-        <p>{ticket.body}</p>
-        <div className={`pill ${ticket.priority}`}>
-          {ticket.priority} priority
-        </div>
-      </div>
-    </main>
-  )
+      <main>
+        <nav>
+          <h2>Ticket Details</h2>
+        </nav>
+        {ticket ? (
+          <div className="card">
+            <h3>{ticket.title}</h3>
+            <small>Created by {ticket.user_email}</small>
+            <p>{ticket.body}</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </main>
+    );
+    
 }
