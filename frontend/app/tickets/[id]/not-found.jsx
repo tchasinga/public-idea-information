e95185e653863c91ab29a91f0fcb6df25748ba22
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-26 18:49:54 +0200

    new code

--- frontend/app/tickets/[id]/not-found.jsx
+++ frontend/app/tickets/[id]/not-found.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <main className="text-center">
       <h2 className="text-3xl">We Hit a Brick Wall.</h2>
       <p>We could not find the ticket you were looking for.</p>
-      <p>Go back to all <Link href="/tickets">tickets</Link>.</p>
+      <p>Go back to all <Link href='/tickets'>tickets</Link>.</p>
     </main>
   )
 }
