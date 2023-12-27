d3479d8901b714dd2434bd8b6aa73f004ba4efab
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-27 17:15:53 +0200

    new chnages

---
+++ frontend/build/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"TOS5aWIi0tMG8DMnBEFE9zDDBIonRbbJzZxJ1RclCVc=\"}"
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    distDir: 'build',
+    reactStrictMode: true,
+    images: {
+      domains: ['https://imgs.search.brave.com/GMUx8P3lW7Mw6iL0Is3cCVqG6tXIBRJjMW91gAXkZbk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/ZnJlZWNvZGVjYW1w/Lm9yZy9uZXdzL2Nv/bnRlbnQvaW1hZ2Vz/L3NpemUvdzIwMDAv/MjAxOS8xMS9Hcm91/cC0xLnBuZw'],
+    },
 
-module.exports = nextConfig
+  }
+  
+  module.exports = nextConfig
