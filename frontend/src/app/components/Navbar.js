2fcfb50c6af72d85a765f608ebdfda6b117ecbd7
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-27 13:51:34 +0200

    new code is added

---
+++ frontend/src/app/components/Navbar.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+
+export default function Navbar() {
+  return (
+    <nav>
+   
+      <h1>Get info</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
+  )
+}
