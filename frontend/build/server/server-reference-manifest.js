adf06cfc7439c11ae7f5bbbf0bc646baf623e933
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-27 17:01:26 +0200

    new commit is added now

---
+++ frontend/build/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"BbXORgVa8I36JERdhiwxvHDI5QYyN5fYjQuyeWIibrw=\"}"
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    distDir: 'build', // Change 'build' to your preferred directory name
+    // Other configurations...
+    reactStrictMode: true, // Enable React strict mode
+    poweredByHeader: false, // Disable the X-Powered-By header for security reasons
+    webpack5: true, // Use Webpack 5 for building
+    images: {
+      domains: ['https://imgs.search.brave.com/GMUx8P3lW7Mw6iL0Is3cCVqG6tXIBRJjMW91gAXkZbk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/ZnJlZWNvZGVjYW1w/Lm9yZy9uZXdzL2Nv/bnRlbnQvaW1hZ2Vz/L3NpemUvdzIwMDAv/MjAxOS8xMS9Hcm91/cC0xLnBuZw'], // Add domains for image optimization
+    },
+    // Add any other Next.js configurations you may need
+  };
+  
