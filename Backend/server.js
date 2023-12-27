c8e66815313d9a1e8f9717da664469a9de4f767f
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-27 16:45:58 +0200

    new code is now added

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ app.use((err, req, res, next) => {
 // Middlewares to allow CORS
 app.use(
   cors({
-    origin: 'http://localhost:3000', 
+    origin: 'https://public-ideas-information.onrender.com', 
     credentials: true,
   })
 );
