ade897a844b7b2a4cbd045e6d872718cc5d3d171
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-27 17:36:54 +0200

    trying again

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
     distDir: 'build', // Change 'build' to your preferred directory name
   reactStrictMode: true, // Enable React strict mode
   poweredByHeader: false, // Disable the X-Powered-By header for security reasons
-  images: {
-    domains: ['imgs.search.brave.com'],
-  },
   }
   
   module.exports = nextConfig
