2b7ce92dbc13d46a20991a80e00a0f2cd4d53b3d
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-27 12:29:08 +0200

    new code is added

--- frontend/src/app/tickets/page.js
+++ frontend/src/app/tickets/page.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import TicketList from './TickectList'
 
 export default function Tickets() {
-  return (
-    <div>
-        <h1>This is my first page...</h1>
-    </div>
-  )
+    return (
+        <main>
+          <nav>
+            <div>
+              <h2>Tickets</h2>
+              <p><small>Currently open tickets.</small></p>
+            </div>
+            {/* <Link href="/tickets/create" className="ml-auto">
+              <button className="btn-primary">New Ticket</button>
+            </Link> */}
+          </nav>
+            <TicketList />
+        </main>
+      )
 }
