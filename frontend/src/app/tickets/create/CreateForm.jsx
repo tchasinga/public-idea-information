5929cb5cde825b488f85c7dc4104e5ef94c2bc73
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-27 22:36:20 +0200

    New code is aded

--- frontend/src/app/tickets/create/CreateForm.jsx
+++ frontend/src/app/tickets/create/CreateForm.jsx
@@ -1,47 +1,45 @@
-"use client"
+"use client";
 
-import { useRouter } from "next/navigation"
-import { useState } from "react"
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function CreateForm() {
-  const router = useRouter()
+  const router = useRouter();
 
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
-  const [priority, setPriority] = useState('low')
-  const [isLoading, setIsLoading] = useState(false)
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [priority, setPriority] = useState("low");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = async (e)  => {
-    e.preventDefault()
-    setIsLoading(true)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
 
-    const newTicket = { title, body, priority, user_email: 'mario@netninja.dev' }
+    const newTicket = {
+      title,
+      body,
+      priority,
+      user_email: "mario@netninja.dev",
+    };
 
-    const res =await fetch('http://localhost:5000/api/creating', {
+    const res = await fetch("http://localhost:5000/api/creating", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newTicket),
     });
 
-    if (!res.ok) {
-      console.error('Failed to fetch:', res.statusText);
-    } else {
-      console.log('Success.....');
-    }
-
     if (res.status === 201) {
-      router.refresh()
-      router.push('/tickets')
+      router.refresh();
+      router.push("/tickets");
     }
-    
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit} className="w-1/2">
       <label>
         <span>Title:</span>
         <input
-          required 
+          required
           type="text"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
@@ -57,22 +55,16 @@ export default function CreateForm() {
       </label>
       <label>
         <span>Priority:</span>
-        <select 
-          onChange={(e) => setPriority(e.target.value)}
-          value={priority}
-        >
+        <select onChange={(e) => setPriority(e.target.value)} value={priority}>
           <option value="low">Low Priority</option>
           <option value="medium">Medium Priority</option>
           <option value="high">High Priority</option>
         </select>
       </label>
-      <button 
-        className="btn-primary" 
-        disabled={isLoading}
-      >
-      {isLoading && <span>Adding...</span>}
-      {!isLoading && <span>Add Ticket</span>}
-    </button>
+      <button className="btn-primary" disabled={isLoading}>
+        {isLoading && <span>Adding...</span>}
+        {!isLoading && <span>Add Ticket</span>}
+      </button>
     </form>
-  )
+  );
 }
