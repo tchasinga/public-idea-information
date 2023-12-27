3d92d2d3f0f745d33ac8b6d35614d549281597f4
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-27 22:23:33 +0200

    Code

--- frontend/src/app/tickets/create/CreateForm.jsx
+++ frontend/src/app/tickets/create/CreateForm.jsx
@@ -17,11 +17,17 @@ export default function CreateForm() {
 
     const newTicket = { title, body, priority, user_email: 'mario@netninja.dev' }
 
-    const res = await fetch('http://localhost:5000/api/creating', {
+    const res =await fetch('http://localhost:5000/api/creating', {
       method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(newTicket)
-    })
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTicket),
+    });
+
+    if (!res.ok) {
+      console.error('Failed to fetch:', res.statusText);
+    } else {
+      console.log('Success.....');
+    }
 
     if (res.status === 201) {
       router.refresh()
