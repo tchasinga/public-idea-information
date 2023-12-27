128afacbf0ae76550ee10cc831a001781e431551
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-27 17:30:07 +0200

    I'll do it again

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true, // Enable React strict mode
   poweredByHeader: false, // Disable the X-Powered-By header for security reasons
   images: {
-    domains: ['https://imgs.search.brave.com/GMUx8P3lW7Mw6iL0Is3cCVqG6tXIBRJjMW91gAXkZbk/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/ZnJlZWNvZGVjYW1w/Lm9yZy9uZXdzL2Nv/bnRlbnQvaW1hZ2Vz/L3NpemUvdzIwMDAv/MjAxOS8xMS9Hcm91/cC0xLnBuZw'], // Add domains for image optimization
+    domains: ['imgs.search.brave.com'],
   },
   }
   
