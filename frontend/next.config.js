a075e1975fe07bac96045069009948b6b5163ed7
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-27 17:37:24 +0200

    trying again

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build', // Change 'build' to your preferred directory name
-  reactStrictMode: true, // Enable React strict mode
-  poweredByHeader: false, // Disable the X-Powered-By header for security reasons
+    distDir: 'build', 
+  reactStrictMode: true, 
+  poweredByHeader: false, 
   }
   
   module.exports = nextConfig
