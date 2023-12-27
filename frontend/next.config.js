f88bdc15e45f0b0053080db93f77ec823c704463
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-27 23:17:38 +0200

    new checks

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir : '.next',
-  reactStrictMode: true,
-  images: {
-    domains: ['https://papaya-pothos-9daf92.netlify.app'],
-  },
 }
   
   module.exports = nextConfig
